5d67bb89f566a7308de86b828584e68936b81e24
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-29 13:16:35 -0600

    Add author and publication year methods

--- lib/book.rb
+++ lib/book.rb
@@ -2,5 +2,16 @@ class Book
   attr_reader :title
   def initialize(book)
     @title = book[:title]
+    @author_first_name = book[:author_first_name]
+    @author_last_name = book[:author_last_name]
+    @publication_date = book[:publication_date]
+  end
+
+  def author
+    @author_first_name + " " + @author_last_name
+  end
+
+  def publication_year
+    @publication_date.slice(9..12)
   end
 end
