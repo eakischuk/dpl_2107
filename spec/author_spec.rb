045590df225e57e7233420a480eab1c63eeb21cc
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-29 13:56:21 -0600

    Confirm author can write multiple books

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -26,5 +26,9 @@ RSpec.describe Author do
     expect(jane_eyre.title).to eq("Jane Eyre")
   end
 
-
+  it 'can write multiple books' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = @charlotte_bronte.write("Villette", "1853")
+    expect(@charlotte_bronte.books).to eq([jane_eyre, villette])
+  end
 end
