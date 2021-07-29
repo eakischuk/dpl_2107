9047b862871d80e0b764dea38894d2da8a202d19
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-29 14:34:25 -0600

    Add test will not checkout nonexistent books

--- spec/library_spec.rb
+++ spec/library_spec.rb
@@ -53,4 +53,13 @@ RSpec.describe Library do
     expect(@dpl.publication_time_frame_for(@charlotte_bronte)).to eq({start: "1847", end: "1857"})
     expect(@dpl.publication_time_frame_for(@harper_lee)).to eq({start: "1960", end: "1960"})
   end
+
+  it 'will not checkout nonexistent books' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    villette = @charlotte_bronte.write("Villette", "1853")
+    mockingbird = @harper_lee.write("To Kill a Mockingbird", "July 11, 1960")
+
+    expect(@dpl.checkout(mockingbird)).to eq(false)
+    expect(@dpl.checkout(jane_eyre)).to eq(false)
+  end
 end
