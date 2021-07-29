82bacd1dd0845d17bc5bc50efb887579ad34a6da
Erika Kischuk <erika.kischuk@gmail.com>
2021-07-29 13:36:31 -0600

    Add write method test

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -13,4 +13,14 @@ RSpec.describe Author do
   it 'has a name' do
     expect(@charlotte_bronte.name).to eq("Charlotte Bronte")
   end
+
+  it 'starts without books' do
+    expect(@charlotte_bronte.books).to eq([])
+  end
+
+  it 'can write books' do
+    jane_eyre = @charlotte_bronte.write("Jane Eyre", "October 16, 1847")
+    # binding.pry
+    expect(@charlotte_bronte.books).to eq(jane_eyre)
+  end
 end
